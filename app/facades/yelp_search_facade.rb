e57f433db9c23705a5080c07a52f222f02dc8dc4
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-23 08:26:59 -0700

    Yelp ssearch facade to handle 'find events' external Yelp api call

---
+++ app/facades/yelp_search_facade.rb
@@ -0,0 +1,20 @@
+class YelpSearchFacade
+  def initialize(location, price="1")
+    @location = location
+    @price = price
+  end
+
+  def search_results
+    event_service = EventService.new
+    res = event_service.get_all_events(@location, @price)
+  end
+
+  def parsed_results
+    result = search_results.map do |event_obj|
+      { 
+        name: event_obj[:name],
+        location: event_obj[:location][:address1]
+      }
+    end
+  end
+end
