018e171bc5a0b4e76fce19e9279fd0ad4017dd22
Miranda Delaney <delaney.miranda1@gmail.com>
2024-02-23 10:13:28 -0700

    add testing and controller for user create and index

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper' 
 
 RSpec.describe 'Api::V1::Users', type: :request do
+  describe "Users Index" do
+    it "sends a list of users" do
+      create_list(:user, 5)
+
+      get api_v1_users_path
+
+      expect(response).to be_successful
+
+      users = JSON.parse(response.body, symbolize_names: true)
+      expect(users.count).to eq(5)
+
+      users.each do |user|
+        expect(users).to be_an(Array)
+
+        expect(user).to have_key(:id)
+        expect(user[:id]).to be_an(Integer)
+
+        expect(user).to have_key(:name)
+        expect(user[:name]).to be_a(String)
+
+        expect(user).to have_key(:email)
+        expect(user[:email]).to be_a(String)
+
+        expect(user).to have_key(:password_digest)
+        expect(user[:password_digest]).to be_a(String)
+      end
+    end
+  end
+
   describe "Users Show" do
     it 'displays the users information' do
       user = create(:user)
@@ -24,6 +53,26 @@ RSpec.describe 'Api::V1::Users', type: :request do
       expect(user_object).to have_key(:email)
       expect(user_object[:email]).to be_an(String)
 
+    end
+  end
+
+  describe "Users Create" do
+    it 'creates a user' do
+      user_params = ({
+        name: 'Bob Dylan',
+        email: 'bob123@gmail.com',
+        password: "likearollingstone123"
+      })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post api_v1_users_path, headers: headers, params: JSON.generate(user: user_params)
+
+      new_user = User.last
+
+      expect(response).to be_successful
+      expect(new_user.name).to eq(user_params[:name])
+      expect(new_user.email).to eq(user_params[:email])
+      expect(new_user.password).to eq(user_params[:password_digest])
     end
   end
 end
