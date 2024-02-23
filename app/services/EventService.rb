499638fd972d7ddd9e2a62b556b9dd5105eec4bb
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-23 08:29:33 -0700

    Event service to handle getting local events from Yelp API

---
+++ app/services/EventService.rb
@@ -0,0 +1,12 @@
+require "faraday"
+
+class EventService
+  def get_all_events(location, price)
+    res = conn.get("businesses/search?sort_by=best_match&location=#{location}&limit=5&price=#{price}")
+    JSON.parse(res.body, symbolize_names: true)[:businesses]
+  end
+
+  def conn = Faraday.new(url: "https://api.yelp.com/v3/") do |faraday|
+    faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.yelp[:key]}"
+  end
+end
