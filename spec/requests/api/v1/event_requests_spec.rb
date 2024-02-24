b6ca827ba0fcb605067c98219e337a3cbbcfded2
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-24 16:14:04 -0700

    Add `price` and `limit` as Yelp API search attributes

--- spec/requests/api/v1/event_requests_spec.rb
+++ spec/requests/api/v1/event_requests_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "Api::V1::Events", type: :request do
   describe "GET /api/v1/events" do
     it "returns cheap YELP events local to the area provided by zip" do
       zip = "80111"
+      price = "1"
+      limit = "6"
 
-      get "#{api_v1_events_path}?location=#{zip}"
+      get "#{api_v1_events_path}?location=#{zip}&price=#{price}&limit=#{limit}"
       
       expect(response).to have_http_status(:success)
       json_response = JSON.parse(response.body, symbolize_names: true)
