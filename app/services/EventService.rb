746a990941d9c362e94b6a45e93fecda4ed70c3c
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-24 16:12:58 -0700

    Add 'limit' attribute to determine how many search results should be returned from search

--- app/services/EventService.rb
+++ app/services/EventService.rb
@@ -1,8 +1,8 @@
 require "faraday"
 
 class EventService
-  def get_all_events(location, price)
-    res = conn.get("businesses/search?sort_by=best_match&location=#{location}&limit=5&price=#{price}")
+  def get_all_events(location, price, limit)
+    res = conn.get("businesses/search?sort_by=best_match&location=#{location}&limit=#{limit}&price=#{price}")
     JSON.parse(res.body, symbolize_names: true)[:businesses]
   end
 
