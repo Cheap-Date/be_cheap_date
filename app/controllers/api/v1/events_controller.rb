a25e214497acbe8d56f5a4341b0f5befef854eca
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-24 16:08:52 -0700

    Add event_params to more quickly and safely pass a user's search parameters to the new YelpSearchFacade object.

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::EventsController < ApplicationController
   def index
-    results = YelpSearchFacade.new(params[:location]).parsed_results
+    results = YelpSearchFacade.new(event_params).parsed_results
     render json: EventSerializer.new.serialize_json(results)
+  end
+
+  private
+
+  def event_params
+    params.permit(:location, :price, :limit)
   end
 end
