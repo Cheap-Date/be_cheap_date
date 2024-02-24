3e89c5d2076e4c1442d52e61db29210001d9f9a1
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-23 17:36:27 -0700

    events table added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_015300) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_025058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
