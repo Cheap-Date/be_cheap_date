fbe78f7bffaa54b6cc4a4139acc23873fb740c60
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-23 17:29:50 -0700

    Re-merge CreateEvents migration

---
+++ db/migrate/20240223025058_create_events.rb
@@ -0,0 +1,10 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
