b9988f1081e634185157f8c1d8282937d2c56d40
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-25 09:05:14 -0700

    Remove price search option. Price should always be '$'.

--- spec/requests/api/v1/event_requests_spec.rb
+++ spec/requests/api/v1/event_requests_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe "Api::V1::Events", type: :request do
   describe "GET /api/v1/events" do
     it "returns cheap YELP events local to the area provided by zip" do
       zip = "80111"
-      price = "1"
-      limit = "6"
+      limit = "5"
 
-      get "#{api_v1_events_path}?location=#{zip}&price=#{price}&limit=#{limit}"
-      
+      get "#{api_v1_events_path}?location=#{zip}&price=1&limit=#{limit}"
+
       expect(response).to have_http_status(:success)
       json_response = JSON.parse(response.body, symbolize_names: true)
     end
