9660364a1b25f9e44e0899daabf19b84ff2299b2
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-25 16:24:26 -0700

    Add more tests to event request spec, ensuring both the location (zip code) and limit (# events) values in the query parameter are returning the accurate area of the events along with the correct number of events

--- spec/requests/api/v1/event_requests_spec.rb
+++ spec/requests/api/v1/event_requests_spec.rb
@@ -1,15 +1,41 @@
 require "rails_helper"
 
-RSpec.describe "Api::V1::Events", type: :request do 
-  describe "GET /api/v1/events" do
-    it "returns cheap YELP events local to the area provided by zip" do
-      zip = "80111"
-      limit = "5"
+RSpec.describe "Api::V1::Events", type: :request do
+  before(:each) do
 
-      get "#{api_v1_events_path}?location=#{zip}&price=1&limit=#{limit}"
+    # Anyone old enough will recognize this as a Beverly Hills (i.e. a well-known and affluent section of Los Angeles
+
+    @zip = "90210"
+    @limit = 7
+  end
+  describe "GET /api/v1/events" do
+    it "returns cheap YELP events given a `zipcode` and response `limit`" do
+      get "#{api_v1_events_path}?location=#{@zip}&price=1&limit=#{@limit}"
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to have_http_status(:success)
+      expect(json_response[:data].count).to eq(7)
+
+      json_response[:data].each do |event|
+        expect(event[:location].last.split(", ").first) == "Los Angeles"
+      end
+
+      # Change zipcode and limit. 80212 covers an area in Denver, CO
+
+      @zip = "80212"
+      @limit = 4
+
+      get "#{api_v1_events_path}?location=#{@zip}&price=1&limit=#{@limit}"
+
       json_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(json_response[:data].count).to eq(4)
+
+      json_response[:data].each do |event|
+        expect(event[:location].last.split(", ").first) == "Denver"
+      end
     end
   end
 end
