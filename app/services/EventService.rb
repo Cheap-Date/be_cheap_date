3c845569c309f65abb2b1ba58c78dd2eb972c98b
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-25 08:59:12 -0700

    Shorten service name; remove 'price' from search options (will always be '$' as purpose of CheapDate)

--- app/services/EventService.rb
+++ app/services/EventService.rb
@@ -1,8 +1,8 @@
 require "faraday"
 
 class EventService
-  def get_all_events(location, price, limit)
-    res = conn.get("businesses/search?sort_by=best_match&location=#{location}&limit=#{limit}&price=#{price}")
+  def get_events(location, limit)
+    res = conn.get("businesses/search?sort_by=best_match&location=#{location}&price=1&limit=#{limit}")
     JSON.parse(res.body, symbolize_names: true)[:businesses]
   end
 
