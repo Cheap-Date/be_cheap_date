58c5e5f52f82ba1c9d851d7e0a1eff9df282132a
Nathan Lambertson <lambertones@gmail.com>
2024-02-27 16:17:02 -0600

    Delete pry

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::LocationsController < ApplicationController
     @loc = Location.new(location_params)
     @latitude = params[:location][:latitude]
     @longitude = params[:location][:longitude]
-    require 'pry'; binding.pry
+    
   end
 
   private
