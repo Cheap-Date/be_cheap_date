f020a06fb13f2f33bcb8bc25fdb1a275ff022cca
Nathan Lambertson <lambertones@gmail.com>
2024-02-27 16:16:23 -0600

    Change some tests to work with new data

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,16 +1,10 @@
 class Api::V1::LocationsController < ApplicationController
 
   def create
-    if current_user.present?
-      location = current_user.locations.new(location_params)
-      if location.save
-        render json: { status: 'success', location_id: location.id }, status: :ok
-      else
-        render json: { errors: location.errors.full_messages }, status: :unprocessable_entity
-      end
-    else
-      render json: { error: 'User must be logged in to create locations' }, status: :unauthorized
-    end
+    @loc = Location.new(location_params)
+    @latitude = params[:location][:latitude]
+    @longitude = params[:location][:longitude]
+    require 'pry'; binding.pry
   end
 
   private
