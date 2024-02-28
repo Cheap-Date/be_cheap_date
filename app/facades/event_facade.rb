53fcce003a8cae92881b512ef0c06fdc19c5fe3b
Nathan Lambertson <lambertones@gmail.com>
2024-02-28 15:14:58 -0600

    Add facade for current events

--- app/facades/event_facade.rb
+++ app/facades/event_facade.rb
@@ -4,4 +4,12 @@ class EventFacade
     call = service.find_by_id(id)
     Event.new(call)
   end
+
+  def current_events(location)
+    service = EventService.new
+    call = service.find_current_events(location)
+    call[:events].map do |event|
+      Event.new(event)
+    end
+  end
 end
