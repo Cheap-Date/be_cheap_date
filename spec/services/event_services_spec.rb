5ba50ebf8964efa478d1849709541853ed646012
Nathan Lambertson <lambertones@gmail.com>
2024-02-28 15:15:39 -0600

    Add tests and tapes for current_events logic

--- spec/services/event_services_spec.rb
+++ spec/services/event_services_spec.rb
@@ -8,14 +8,32 @@ RSpec.describe EventService, vcr: true do
   end
 
   describe "methods" do
-    it "has a .get_events method" do
+    it "has a #get_events method" do
       service = EventService.new.get_events(70117, 5)
 
       expect(service).to be_a(Hash)
     end
 
-    it "has a .find_by_id method" do
+    it "has a #find_by_id method" do
       service = EventService.new.find_by_id("san-francisco-peace-love-and-yelp-our-3rd-annual-holiday-party")
+
+      expect(service).to be_a(Hash)
+    end
+  end
+
+  describe "#find_current_events", vcr: false do
+    before do
+      VCR.turn_off!(ignore_cassettes: true)
+      WebMock.allow_net_connect!
+    end
+
+    after do
+      VCR.turn_on!
+      WebMock.disable_net_connect!(allow_localhost: true)
+    end
+
+    it "returns a hash" do
+      service = EventService.new.find_current_events("70117")
 
       expect(service).to be_a(Hash)
     end
