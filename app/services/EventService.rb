601fafa55be8588203c432b72c5df4b3b12f8b22
Nathan Lambertson <lambertones@gmail.com>
2024-02-28 15:28:51 -0600

    Up the limit to 25 from 13 events returned

--- app/services/EventService.rb
+++ app/services/EventService.rb
@@ -10,7 +10,7 @@ class EventService
   end
 
   def find_current_events(location)
-    res = conn.get("events?start_date=#{Time.now.to_i}&location=#{location}&limit=13")
+    res = conn.get("events?start_date=#{Time.now.to_i}&location=#{location}&limit=25")
     JSON.parse(res.body, symbolize_names: true)
   end
 
