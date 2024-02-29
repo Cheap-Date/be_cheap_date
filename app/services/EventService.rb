4a10b7006c598bae8545f82e81578f35704fdc29
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-28 19:57:14 -0700

    Separate zip and lat/lon service methods to make code easier to follow and also adhere to SRP.

--- app/services/EventService.rb
+++ app/services/EventService.rb
@@ -1,8 +1,16 @@
 require "faraday"
 
 class EventService
-  def get_events(location, limit)
+  def get_events_by_zip(location, limit)
     res = conn.get("events?limit=#{limit}&location=#{location}&price=1")
+    JSON.parse(res.body, symbolize_names: true)
+  end
+
+  def get_geo_events(latitude, longitude, limit)
+    res = conn.get("events?limit=#{limit}
+                    &latitude=#{latitude}
+                    &longitude=#{longitude}
+                    &price=1") 
     JSON.parse(res.body, symbolize_names: true)
   end
 
