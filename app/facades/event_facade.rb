e310412dd899f09e2549df6868a2260cfcf3e5f7
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-01 05:10:02 -0700

    Delete YelpSearchFacade in favor of EventFacade. Much cleaner. Took a few methods from YelpFacade but really liked how Nate was implementing event#show along with a couple other actions to hit the same Yelp API and it was much cleaner and centralized.

--- app/facades/event_facade.rb
+++ app/facades/event_facade.rb
@@ -5,10 +5,46 @@ class EventFacade
     EventPoro.new(call)
   end
 
+  def events_by_zip(zip)
+    service = EventService.new
+    call = service.find_by_zip(zip)
+    event_poros = create_event_poros(call)
+  end
+
   def current_events(location)
     service = EventService.new
     call = service.find_current_events(location)
     call[:events].map do |event|
+      EventPoro.new(event)
+    end
+  end
+
+  def events_by_lat_and_long(lat, long)
+    service = EventService.new
+    call = service.find_by_lat_and_long(lat, long)
+    event_poros = create_event_poros(call)
+  end
+
+  def events_by_city_state(city_state)
+    service = EventService.new
+    call = service.find_by_city_state(city_state)
+  end
+
+  def events_with_no_user_input
+    service = EventService.new
+    call = service.gobblers_knob_events
+    event_poros = create_event_poros(call)
+  end
+
+  def valid_latitude_and_longitude?(val)
+    pattern = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/
+    return val.match(pattern)
+  end
+
+  # private
+
+  def create_event_poros(api_response)
+    api_response[:events].map do |event|
       EventPoro.new(event)
     end
   end
