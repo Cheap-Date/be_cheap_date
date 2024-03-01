7981c97263fddb24733bef153dea3313edb4e406
Dylan Perry <dylanbperry@gmail.com>
2024-02-29 16:30:58 -0800

    Clean up tests for new log in format

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,7 @@ class User < ApplicationRecord
   validates :name, :email, presence: :true
   validates :email, uniqueness: :true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
-  # validates_presence_of :password_digest
 
   has_many :meetups
   has_many :locations
-
-  # has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,6 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email)}
     it { should allow_value('user@example.com').for(:email) }
     it { should_not allow_value('invalid_email').for(:email) }
-    it { should validate_presence_of(:password_digest)}
     it { should have_many(:meetups) }
-  end
-
-  describe 'has_secure_password' do
-    it { should have_secure_password }
   end
 end
